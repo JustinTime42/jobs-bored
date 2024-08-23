af22a2650a906b7a005f3afa01cf8105aab3680c
JustinTime42 <justincase18@msn.com>
2024-08-23 13:19:11 -0800

    fixed bugs in supabase integration

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from 'next/server'
-import { updateSession } from '@/utils/supabase/middleware'
+import { updateSession } from './src/utils/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request)
--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 // The client you created from the Server-Side Auth instructions
-import { createClient } from '@/src/utils/supabase/server'
+import { supabaseAdmin } from '@/src/utils/supabase/admin'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
@@ -9,8 +9,7 @@ export async function GET(request: Request) {
   const next = searchParams.get('next') ?? '/'
 
   if (code) {
-    const supabase = createClient()
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    const { error } = await supabaseAdmin.auth.exchangeCodeForSession(code)
     if (!error) {
       const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === 'development'
