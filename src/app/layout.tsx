a8c30e6c6ec90c0da54e9a1f848411d61f244b7e
JustinTime42 <justincase18@msn.com>
2024-08-23 14:45:38 -0800

    added favicon and logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Jobs Bored",
+  description: "Tools to help you get a job in tech",
 };
 
 export default function RootLayout({
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import AuthButton from "./AuthButton";
 import { useEffect } from "react";
 import { useUserContext } from "@/src/app/context/UserContext";
+import Image from "next/image";
 
 export default function Header() {
   const { user, loading, error, fetchUser } = useUserContext();
@@ -13,6 +14,7 @@ export default function Header() {
 
   return (
       <nav className="w-full flex justify-center border-b border-b-foreground/10">
+        <Image src="/logo.png" alt="Logo" width={128} height={128} />
         <div className="w-full max-w-4xl flex justify-end gap-10 items-center p-3 text-sm">
           {user && (
             <Link href="/dashboard">
