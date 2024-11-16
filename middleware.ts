1fcc5a69b86b221541603de77522970f367c27c0
JustinTime42 <justincase18@msn.com>
2024-11-15 18:12:36 -0900

    debugging auth redirect

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
 import { type NextRequest } from 'next/server'
+import { NextResponse } from 'next/server';
 import { updateSession } from './src/utils/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/api/stripe-webhook')) {
+    return NextResponse.next();
+  }
   return await updateSession(request)
 }
 
