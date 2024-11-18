59886c9db8b7d39f358c8776ebf843af0dc47488
JustinTime42 <justincase18@msn.com>
2024-11-18 12:44:14 -0900

    testing webhook

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
-import { type NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
-import { updateSession } from './src/utils/supabase/middleware';
+// import { type NextRequest } from 'next/server';
+// import { NextResponse } from 'next/server';
+// import { updateSession } from './src/utils/supabase/middleware';
 
-export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/api/stripe-webhook') {
-    return NextResponse.next(); // Skip all middleware logic
-  }
+// export async function middleware(request: NextRequest) {
+//   if (request.nextUrl.pathname === '/api/stripe-webhook') {
+//     return NextResponse.next(); // Skip all middleware logic
+//   }
 
-  return await updateSession(request); // Continue normal middleware
-}
+//   return await updateSession(request); // Continue normal middleware
+// }
 
-export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|api/stripe-webhook).*)', // Exclude stripe-webhook
-  ],
-};
+// export const config = {
+//   matcher: [
+//     '/((?!_next/static|_next/image|favicon.ico|api/stripe-webhook).*)', // Exclude stripe-webhook
+//   ],
+// };
--- src/utils/supabase/admin.ts
+++ src/utils/supabase/admin.ts
@@ -18,3 +18,4 @@ export const createAdminClient = () => {
 
 export const supabaseAdmin = createAdminClient();
   
+
